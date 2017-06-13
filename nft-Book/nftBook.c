e39a172c7fe23ddc35e5a74367a5e72dd6bb39f4
Zhiyi Zhang <zhiyi@cs.ucla.edu>
2017-06-13 13:10:45 -0700

    no change

--- nft-Book/nftBook.c
+++ nft-Book/nftBook.c
@@ -47,8 +47,8 @@
 
 // Preferences.
 static int prefWindowed = TRUE;           // Use windowed (TRUE) or fullscreen mode (FALSE) on launch.
-static int prefWidth = 320;               // Preferred initial window width.
-static int prefHeight = 240;              // Preferred initial window height.
+static int prefWidth = 640;               // Preferred initial window width.
+static int prefHeight = 480;              // Preferred initial window height.
 static int prefDepth = 16;                // Fullscreen mode bit depth. Set to 0 to use default depth.
 static int prefRefresh = 0;               // Fullscreen mode refresh rate. Set to 0 to use default rate.
 
@@ -116,36 +116,36 @@ static void Display(void);
 
 int main(int argc, char** argv)
 {
-    
+
     /* receiving UDP packets from sender client */
     if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("cannot create socket\n");
         return 0;
     }
-    
+
     if ((send_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         perror("cannot create socket\n");
         return 0;
     }
-    
+
     /* bind the socket to any valid IP address and a specific port */
     memset((char *)&myaddr, 0, sizeof(myaddr));
     myaddr.sin_family = AF_INET;
     myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     myaddr.sin_port = htons(SERVICE_PORT);
-    
+
     memset((char *)&dstaddr, 0, sizeof(dstaddr));
     dstaddr.sin_family = AF_INET;
     dstaddr.sin_addr.s_addr = inet_addr("192.168.2.5");
     //dstaddr.sin_addr.s_addr = inet_addr("131.179.210.120");
     dstaddr.sin_port = htons(10001);
-    
-    
+
+
     if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
         perror("bind failed");
         return 0;
     }
-    
+
     char    glutGamemode[32] = "";
     char   *vconf = "-device=Dummy -width=176 -height=144 -format=RGBA";
     char    cparaDefault[] = "Data2/camera_para.dat";
@@ -154,17 +154,17 @@ int main(int argc, char** argv)
     int     gotTwoPartOption;
     const char markerConfigDataFilename[] = "Data2/markers.dat";
     const char objectDataFilename[] = "Data2/objects.dat";
-    
+
 #ifdef DEBUG
     arLogLevel = AR_LOG_LEVEL_DEBUG;
 #endif
-    
+
     //
     // Process command-line options.
     //
-    
+
     glutInit(&argc, argv);
-    
+
     i = 1; // argv[0] is name of app, so start at 1.
     while (i < argc) {
         gotTwoPartOption = FALSE;
@@ -221,30 +221,30 @@ int main(int argc, char** argv)
         }
         i++;
     }
-    
-    
+
+
     //
     // Video setup.
     //
-    
+
     if (!setupCamera((cpara ? cpara : cparaDefault), vconf, &gCparamLT)) {
         ARLOGe("main(): Unable to set up AR camera.\n");
         exit(-1);
     }
-    
+
     //
     // AR init.
     //
-    
+
     if (!initNFT(gCparamLT, arVideoGetPixelFormat())) {
         ARLOGe("main(): Unable to init NFT.\n");
         exit(-1);
     }
-    
+
     //
     // Markers setup.
     //
-    
+
     // Load marker(s).
     newMarkers(markerConfigDataFilename, &markersNFT, &markersNFTCount);
     if (!markersNFTCount) {
@@ -253,18 +253,18 @@ int main(int argc, char** argv)
         exit(-1);
     }
     ARLOGi("Marker count = %d\n", markersNFTCount);
-    
+
     // Marker data has been loaded, so now load NFT data.
     if (!loadNFTData()) {
         ARLOGe("Error loading NFT data.\n");
         cleanup();
         exit(-1);
     }
-    
+
     //
     // Graphics setup.
     //
-    
+
     // Set up GL context(s) for OpenGL to draw into.
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
     if (prefWindowed) {
@@ -294,42 +294,42 @@ int main(int argc, char** argv)
             glutFullScreen();
         }
     }
-    
+
     // Create the OpenGL projection from the calibrated camera parameters.
     arglCameraFrustumRH(&(gCparamLT->param), VIEW_DISTANCE_MIN, VIEW_DISTANCE_MAX, cameraLens);
     cameraPoseValid = FALSE;
-    
+
     // Setup ARgsub_lite library for current OpenGL context.
     if ((gArglSettings = arglSetupForCurrentContext(&(gCparamLT->param), arVideoGetPixelFormat())) == NULL) {
         ARLOGe("main(): arglSetupForCurrentContext() returned error.\n");
         cleanup();
         exit(-1);
     }
-    
+
     // Load objects (i.e. OSG models).
     VirtualEnvironmentInit(objectDataFilename);
     VirtualEnvironmentHandleARViewUpdatedCameraLens(cameraLens);
-    
+
     //
     // Setup complete. Start tracking.
     //
-    
+
     // Start the video.
     if (arVideoCapStart() != 0) {
         ARLOGe("setupCamera(): Unable to begin camera data capture.\n");
         return (FALSE);
     }
     arUtilTimerReset();
-    
+
     // Register GLUT event-handling callbacks.
     // NB: mainLoop() is registered by Visibility.
     glutDisplayFunc(Display);
     glutReshapeFunc(Reshape);
     glutVisibilityFunc(Visibility);
     glutKeyboardFunc(Keyboard);
-    
+
     glutMainLoop();
-    
+
     return (0);
 }
 
@@ -354,31 +354,31 @@ static int setupCamera(const char *cparam_name, char *vconf, ARParamLT **cparamL
     ARParam         cparam;
     int             xsize, ysize;
     AR_PIXEL_FORMAT pixFormat;
-    
+
     // Open the video path.
     if (arVideoOpen(vconf) < 0) {
         ARLOGe("setupCamera(): Unable to open connection to camera.\n");
         return (FALSE);
     }
-    
+
     // Find the size of the window.
     if (arVideoGetSize(&xsize, &ysize) < 0) {
         ARLOGe("setupCamera(): Unable to determine camera frame size.\n");
         arVideoClose();
         return (FALSE);
     }
-    
+
     ARLOGi("Camera image size (x,y) = (%d,%d)\n", xsize, ysize);
-    
+
     // Get the format in which the camera is returning pixels.
     pixFormat = arVideoGetPixelFormat();
-    
+
     if (pixFormat == AR_PIXEL_FORMAT_INVALID) {
         ARLOGe("setupCamera(): Camera is using unsupported pixel format.\n");
         arVideoClose();
         return (FALSE);
     }
-    
+
     // Load the camera parameters, resize for the window and init.
     if (arParamLoad(cparam_name, 1, &cparam) < 0) {
         ARLOGe("setupCamera(): Error loading parameter file %s for camera.\n", cparam_name);
@@ -398,7 +398,7 @@ static int setupCamera(const char *cparam_name, char *vconf, ARParamLT **cparamL
         arVideoClose();
         return (FALSE);
     }
-    
+
     return (TRUE);
 }
 
@@ -409,7 +409,7 @@ static int initNFT(ARParamLT *cparamLT, AR_PIXEL_FORMAT pixFormat)
     //
     // NFT init.
     //
-    
+
     // KPM init.
     kpmHandle = kpmCreateHandle(cparamLT, pixFormat);
     if (!kpmHandle) {
@@ -417,7 +417,7 @@ static int initNFT(ARParamLT *cparamLT, AR_PIXEL_FORMAT pixFormat)
         return (FALSE);
     }
     //kpmSetProcMode( kpmHandle, KpmProcHalfSize );
-    
+
     // AR2 init.
     if( (ar2Handle = ar2CreateHandle(cparamLT, pixFormat, AR2_TRACKING_DEFAULT_THREAD_NUM)) == NULL ) {
         ARLOGe("Error: ar2CreateHandle.\n");
@@ -449,7 +449,7 @@ static int initNFT(ARParamLT *cparamLT, AR_PIXEL_FORMAT pixFormat)
 static int unloadNFTData(void)
 {
     int i, j;
-    
+
     if (threadHandle) {
         ARLOGi("Stopping NFT2 tracking thread.\n");
         trackingInitQuit(&threadHandle);
@@ -462,7 +462,7 @@ static int unloadNFTData(void)
     }
     if (j > 0) ARLOGi("Unloaded %d NFT tracking surfaces.\n", j);
     surfaceSetCount = 0;
-    
+
     return 0;
 }
 
@@ -472,7 +472,7 @@ static int loadNFTData(void)
 {
     int i;
     KpmRefDataSet *refDataSet;
-    
+
     // If data was already loaded, stop KPM tracking thread and unload previously loaded data.
     if (threadHandle) {
         ARLOGi("Reloading NFT data.\n");
@@ -480,9 +480,9 @@ static int loadNFTData(void)
     } else {
         ARLOGi("Loading NFT data.\n");
     }
-    
+
     refDataSet = NULL;
-    
+
     for (i = 0; i < markersNFTCount; i++) {
         // Load KPM data.
         KpmRefDataSet  *refDataSet2;
@@ -503,15 +503,15 @@ static int loadNFTData(void)
             exit(-1);
         }
         ARLOGi("  Done.\n");
-        
+
         // Load AR2 data.
         ARLOGi("Reading %s.fset\n", markersNFT[i].datasetPathname);
-        
+
         if ((surfaceSet[surfaceSetCount] = ar2ReadSurfaceSet(markersNFT[i].datasetPathname, "fset", NULL)) == NULL ) {
             ARLOGe("Error reading data from %s.fset\n", markersNFT[i].datasetPathname);
         }
         ARLOGi("  Done.\n");
-        
+
         surfaceSetCount++;
         if (surfaceSetCount == PAGES_MAX) break;
     }
@@ -520,11 +520,11 @@ static int loadNFTData(void)
         exit(-1);
     }
     kpmDeleteRefDataSet(&refDataSet);
-    
+
     // Start the KPM tracking thread.
     threadHandle = trackingInitInit(kpmHandle);
     if (!threadHandle) exit(-1);
-    
+
     ARLOGi("Loading of NFT data complete.\n");
     return (TRUE);
 }
@@ -532,20 +532,20 @@ static int loadNFTData(void)
 static void cleanup(void)
 {
     VirtualEnvironmentFinal();
-    
+
     if (markersNFT) deleteMarkers(&markersNFT, &markersNFTCount);
-    
+
     // NFT cleanup.
     unloadNFTData();
     ARLOGd("Cleaning up ARToolKit NFT handles.\n");
     ar2DeleteHandle(&ar2Handle);
     kpmDeleteHandle(&kpmHandle);
     arParamLTFree(&gCparamLT);
-    
+
     // OpenGL cleanup.
     arglCleanup(gArglSettings);
     gArglSettings = NULL;
-    
+
     // Camera cleanup.
     arVideoCapStop();
     arVideoClose();
@@ -579,10 +579,10 @@ static void mainLoop(void)
     int ms;
     float s_elapsed;
     ARUint8 *image;
-    
-    
+
+
     int             i, j, k;
-    
+
     unsigned char buf[BUFSIZE];
     ARUint8 * whole_frame = (ARUint8 *)malloc(frame_buffer_size);
     int last_id = 0;
@@ -610,10 +610,10 @@ static void mainLoop(void)
             //            *last_segment_tag = 0;
             //            memcpy(last_segment_tag, buf + 3, 1);
             //            last_id = *last_segment_tag;
-            
+
             memcpy(whole_frame+total_size_received, buf+6, recvlen-6);
             total_size_received += recvlen-6;
-            
+
             short* frame_id = (short*)malloc(sizeof(short));
             *frame_id = 0;
             memcpy(frame_id, buf, 2);
@@ -624,7 +624,7 @@ static void mainLoop(void)
             *last_segment_tag = 0;
             memcpy(last_segment_tag, buf + 4, 2);
             last_id = *last_segment_tag;
-            
+
             free(frame_id);
             free(segment_id);
             free(last_segment_tag);
@@ -633,22 +633,22 @@ static void mainLoop(void)
         else
             printf("uh oh - something went wrong!\n");
     }
-    
+
     ARLOGe("Received a whole frame of size: %d.\n", total_size_received);
-    
+
     // Calculate time delta.
     ms = glutGet(GLUT_ELAPSED_TIME);
     s_elapsed = (float)(ms - ms_prev) * 0.001f;
     ms_prev = ms;
-    
+
     // Grab a video frame.
     // if ((image = arVideoGetImage()) != NULL) {
     //   gARTImage = image; // Save the fetched image.
-    
+
     if (total_size_received == 101376) {
-        
+
         gARTImage = whole_frame;
-        
+
         // Calculate FPS every 30 frames.
         if (gCallCountMarkerDetect % 30 == 0) {
             gFPS = 30.0/arUtilTimer();
@@ -656,15 +656,15 @@ static void mainLoop(void)
             gCallCountMarkerDetect = 0;
         }
         gCallCountMarkerDetect++; // Increment ARToolKit FPS counter.
-        
-        
+
+
         // Run marker detection on frame
         if (threadHandle) {
             // Perform NFT tracking.
             float            err;
             int              ret;
             int              pageNo;
-            
+
             if( detectedPage == -2 ) {
                 trackingInitStart( threadHandle, gARTImage );
                 detectedPage = -1;
@@ -697,12 +697,12 @@ static void mainLoop(void)
             ARLOGe("Error: threadHandle\n");
             detectedPage = -2;
         }
-        
+
         int sizeofinfo = 0;
         char* marker_client_buffer = (char*)malloc(sizeof(int) + 3*4*sizeof(float) + 1);
         memcpy(marker_client_buffer, &detectedPage, sizeof(int));
         sizeofinfo += sizeof(int);
-        
+
         for (int i = 0; i < 3; i ++)
         {
             for (int j = 0; j < 4; j ++)
@@ -711,19 +711,19 @@ static void mainLoop(void)
                 sizeofinfo += sizeof(float);
             }
         }
-        
+
         marker_client_buffer[sizeofinfo] = '\0';
         sizeofinfo += 1;
         //char marker_client_buffer[20] = "Hello server!";
-        
+
         //ARLOGe("Size of markers: %d\n", markersNFTCount);
         if (sendto(send_fd, marker_client_buffer, sizeofinfo, 0, (struct sockaddr *)&dstaddr, sizeof(dstaddr)) < 0)
         {
             ARLOGe("Sending markers to client failed.\n");
         }
-        
+
         ARLOGe("send successully %d, %f, %f, %f\n", detectedPage, trackingTrans[0][0], trackingTrans[0][2], trackingTrans[2][0]);
-        
+
         // Update markers.
         for (i = 0; i < markersNFTCount; i++) {
             markersNFT[i].validPrev = markersNFT[i].valid;
@@ -733,43 +733,43 @@ static void mainLoop(void)
             }
             else markersNFT[i].valid = FALSE;
             if (markersNFT[i].valid) {
-                
+
                 // Filter the pose estimate.
                 if (markersNFT[i].ftmi) {
                     if (arFilterTransMat(markersNFT[i].ftmi, markersNFT[i].trans, !markersNFT[i].validPrev) < 0) {
                         ARLOGe("arFilterTransMat error with marker %d.\n", i);
                     }
                 }
-                
+
                 if (!markersNFT[i].validPrev) {
                     // Marker has become visible, tell any dependent objects.
                     VirtualEnvironmentHandleARMarkerAppeared(i);
                 }
-                
+
                 // We have a new pose, so set that.
                 arglCameraViewRH((const ARdouble (*)[4])markersNFT[i].trans, markersNFT[i].pose.T, VIEW_SCALEFACTOR);
                 // Tell any dependent objects about the update.
                 VirtualEnvironmentHandleARMarkerWasUpdated(i, markersNFT[i].pose);
-                
+
             } else {
-                
+
                 if (markersNFT[i].validPrev) {
                     // Marker has ceased to be visible, tell any dependent objects.
                     VirtualEnvironmentHandleARMarkerDisappeared(i);
                 }
             }
         }
-        
-        
+
+
         // Tell GLUT the display has changed.
         glutPostRedisplay();
     } else {
         arUtilSleep(2);
     }
-    
+
     free(whole_frame);
-    
-    
+
+
 }
 
 //
@@ -792,10 +792,10 @@ static void Visibility(int visible)
 static void Reshape(int w, int h)
 {
     GLint viewport[4];
-    
+
     gWindowW = w;
     gWindowH = h;
-    
+
     // Call through to anyone else who needs to know about window sizing here.
     viewport[0] = 0;
     viewport[1] = 0;
@@ -808,7 +808,7 @@ static void print(const char *text, const float x, const float y)
 {
     int i;
     size_t len;
-    
+
     if (!text) return;
     len = strlen(text);
     glRasterPos2f(x, y);
@@ -821,15 +821,15 @@ static void print(const char *text, const float x, const float y)
 static void Display(void)
 {
     char text[256];
-    
+
     // Select correct buffer for this context.
     glDrawBuffer(GL_BACK);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the buffers for new frame.
-    
+
     arglPixelBufferDataUpload(gArglSettings, gARTImage);
     arglDispImage(gArglSettings);
     gARTImage = NULL; // Invalidate image data.
-    
+
     // Set up 3D mode.
     glMatrixMode(GL_PROJECTION);
 #ifdef ARDOUBLE_IS_FLOAT
@@ -840,29 +840,29 @@ static void Display(void)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     glEnable(GL_DEPTH_TEST);
-    
+
     // Set any initial per-frame GL state you require here.
     // --->
-    
+
     // Lighting and geometry that moves with the camera should be added here.
     // (I.e. should be specified before camera pose transform.)
     // --->
-    
+
     VirtualEnvironmentHandleARViewDrawPreCamera();
-    
+
     if (cameraPoseValid) {
-        
+
 #ifdef ARDOUBLE_IS_FLOAT
         glMultMatrixf(cameraPose);
 #else
         glMultMatrixd(cameraPose);
 #endif
-        
+
         // All lighting and geometry to be drawn in world coordinates goes here.
         // --->
         VirtualEnvironmentHandleARViewDrawPostCamera();
     }
-    
+
     // Set up 2D mode.
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
@@ -871,17 +871,17 @@ static void Display(void)
     glLoadIdentity();
     glDisable(GL_LIGHTING);
     glDisable(GL_DEPTH_TEST);
-    
+
     // Add your own 2D overlays here.
     // --->
-    
+
     VirtualEnvironmentHandleARViewDrawOverlay();
-    
+
     // Show some real-time info.
     glColor3ub(255, 255, 255);
     // FPS.
     snprintf(text, sizeof(text), "FPS %.1f", gFPS);
     print(text, 2.0f, (float)gWindowH - 12.0f);
-    
+
     glutSwapBuffers();
 }
